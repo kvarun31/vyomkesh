51d3b87d77b8d305c768adafe75ef6ad088cea96
Kumar Varun <kumar@edgerank.com>
2012-02-24 03:32:57 +0530

    WEEK-1: MODIFIED SIMPLE CRAWLER

--- WEEK_1/simple_crawler.py
+++ WEEK_1/simple_crawler.py
@@ -1,3 +1,13 @@
+page = '''
+  <div id="courses-overlay" class="overlay">
+                    <!-- TODO eventually load course list from database -->
+                    <p><a href="/overview/Course/cs101">CS 101: Building a Search Engine</a></p>
+                    <hr/>
+                    <p><a href="/overview/Course/cs373">CS 373: Programming a Robotic Car</a></p>
+                    <hr/>
+                    <p><a href="/" class="arrow">SEE ALL UPCOMING CLASSES</a></p>       
+                </div>
+'''
 def get_urls(html):
     urls = []
     eq = 0
@@ -10,6 +20,10 @@ def get_urls(html):
 
 def get_url(html, start_point):
     start_quote = html.find('"',start_point)
-    end_quote = html.find('"',end_quote)
+    end_quote = html.find('"',start_quote+1)
     url = html[start_quote+1:end_quote]
     return (end_quote,url)
+
+urls = get_urls(page)
+for url in urls:
+    print url
